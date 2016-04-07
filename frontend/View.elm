8c4d0378ab228961c0a6f4816719a59009ac29c0
Frank Bonetti <frankbonetti@kapowevents.com>
2016-04-06 21:35:07 -0500

    cleanup

--- frontend/View.elm
+++ frontend/View.elm
@@ -62,32 +62,3 @@ headerLinkClass model route =
     class "active"
   else
     class ""
-
-
-    --[ h1 [] [ text "Elm TransitRouter example" ]
-    --, div [ class "menu" ]
-    --    [ a (clickTo <| Routes.encode Home) [ text "Home" ]
-    --    , a (clickTo <| Routes.encode Login) [ text "Login" ]
-    --    , a (clickTo <| Routes.encode Register) [ text "Register" ]
-    --    , a (clickTo <| Routes.encode (Page 1)) [ text "Page 1" ]
-    --    , a (clickTo <| Routes.encode (Page 2)) [ text "Page 2" ]
-    --    ]
-    --, div
-    --    [ class "content"
-    --    , style (TransitStyle.fadeSlideLeft 100 (getTransition model))
-    --    ]
-    --    [ case (TransitRouter.getRoute model) of
-    --        Home ->
-    --          text <| "This is home"
-    --        Login ->
-    --          Pages.Login.view (Signal.forwardTo address LoginPageAction) model.loginModel
-    --        Register ->
-    --          Pages.Register.view (Signal.forwardTo address RegisterPageAction) model.registerModel
-    --        User _ ->
-    --          Pages.User.view (Signal.forwardTo address UserPageAction) model.userModel
-    --        Page _ ->
-    --          text <| "This is page " ++ toString model.page
-    --        EmptyRoute ->
-    --          text <| ""
-    --    ]
-    --]
